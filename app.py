816868337188773d14b204aa424329583ca4eb07
oana-afk <157591455+oana-afk@users.noreply.github.com>
2025-05-18 10:15:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ from routes.scene_routes import scene_bp
 from flask_cors import CORS
 from admin import admin_bp  # dacă admin.py e în root
 
-def create_app():
+def create_app(environ=None, start_response=None):
     app = Flask(__name__)
-    CORS(app) 
+    CORS(app)
 
-    # Register blueprint routes modular
+    from routes.ai_routes import ai_bp
+    from routes.quiz_routes import quiz_bp
+    from routes.scene_routes import scene_bp
+    from admin import admin_bp
+
     app.register_blueprint(ai_bp, url_prefix='/api/ai')
     app.register_blueprint(quiz_bp, url_prefix='/api/quiz')
     app.register_blueprint(scene_bp, url_prefix='/api/scene')
@@ -20,7 +24,6 @@ def create_app():
         return {'status': 'SyncPlay backend running 🎬'}
 
     return app
-
 
 if __name__ == '__main__':
     app = create_app()
